93faf816921d4f67949ca7a6e1d8bc12226e030f
jackplo <jackplo53@gmail.com>
2022-12-12 13:27:46 -0500

    Added to Achievements

--- NAND-Mobile/Screens/Achievements.js
+++ NAND-Mobile/Screens/Achievements.js
@@ -1,7 +1,23 @@
 import React, { useState } from "react";
-import { Image, HStack, NativeBaseProvider, extendTheme, Box, Text, Center, Button, Container, Stack} from "native-base";
+import { Image, HStack, NativeBaseProvider, extendTheme, Box, Text, Center, Button, Container, Stack, CheckIcon, View} from "native-base";
 
 export const AchievementsScreen = ({route, navigation}) => {
+
+    const AchievementsJSON = {
+        "Achievements": {
+            1: {
+                "name": "Beat Level 1",
+                "prereq": "TO DO",
+                "status": true,
+            },
+            2: {
+                "name": "Beat Level 2",
+                "prereq": "TO DO",
+                "status": false,
+            }
+
+        }
+    }
 
     const [completed, setCompleted] = useState(() => {
         // TO DO code to calculate percent completed
@@ -26,18 +42,26 @@ export const AchievementsScreen = ({route, navigation}) => {
     */
 
     function showAchievements(id){
+
+        if (AchievementsJSON["Achievements"][id]["status"] === false) {
+            return
+        }
+
         return (
-        <Box width="100%" key={id} bg={"#729EA1"} style={{margin:5, borderRadius:10}}>
-            <Text textAlign={"center"}>{id}</Text>
+        <Box width="100%" key={id} bg={"#D9D9D9"} style={{margin:5, borderRadius:10, flexDirection: "row", justifyContent: "center"}}>
+            <CheckIcon flex={1} size="5" mt="0.5" color="emerald.500"/>
+            <Text flex={2} paddingLeft={10}>{AchievementsJSON["Achievements"][id]["name"]}</Text>
         </Box>)
     }
 
     return (
         <NativeBaseProvider>
-            <Container>
-                <Text>{completed}</Text>
-                <Text>{Object.keys(AchievementsJSON["Achievements"]).map((id) => (showAchievements(id)))}</Text>
-            </Container>
+            <View alignItems={"center"}>
+                <Container>
+                    <Text>{completed}</Text>
+                    <Text>{Object.keys(AchievementsJSON["Achievements"]).map((id) => (showAchievements(id)))}</Text>
+                </Container>
+            </View>
         </NativeBaseProvider>
     )
 }
